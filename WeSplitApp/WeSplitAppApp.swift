82bcbeb9c3531786bbb3be6e7ecf3ade22ca5480
neshwa.parveen <neshwa.parveen@chillarcards.com>
2023-12-12 10:25:30 +0530

    Initial Commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Neshwa on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
